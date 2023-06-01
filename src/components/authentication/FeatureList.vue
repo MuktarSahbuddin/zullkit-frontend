4e80f6f99a7e7801534c65f282717912ca4281f9
MuktarSahbuddin <11751101899@students.uin-suska.ac.id>
2023-06-01 09:17:28 +0700

    update 31 januari login dan register

---
+++ src/components/authentication/FeatureList.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Customizable from "@/components/authentication/features/Customizable.vue";
+import Fortune from "@/components/authentication/features/Fortune.vue";
+import Documentations from "@/components/authentication/features/Documentations.vue";
+</script>
+<template>
+  <Customizable />
+  <Fortune />
+  <Documentations />
+</template>
--- src/components/authentication/RegisterForm.vue
+++ src/components/authentication/RegisterForm.vue
@@ -1,20 +1,32 @@
 <script setup>
-import { ref } from 'vue'
-import {RouterLink} from 'vue-router'
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
 
 const form = ref({
+  'name' :'',
   'email' :'',
   'password' :'',
-})
+});
 </script>
 
 
 <template>
   <form>
     <div class="mb-4">
+      <label class="block mb-1" for="name">Name</label>
+      <input
+      v-model="form.name"
+        placeholder="Type your full name"
+        id="name"
+        type="text"
+        name="name"
+        class="block w-full py-3 mt-2 border border-gray-300 rounded-full shadow-sm px-7 focus:border-indigo-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:bg-gray-100"
+      />
+    </div>
+    <div class="mb-4">
       <label class="block mb-1" for="email">Email Address</label>
       <input
-        v-model="form.email"
+       v-model="form.email"
         placeholder="Type your email"
         id="email"
         type="text"
@@ -25,7 +37,7 @@ const form = ref({
     <div class="mb-4">
       <label class="block mb-1" for="password">Password</label>
       <input
-        v-model="form.password"
+       v-model="form.password"
         placeholder="Type your password"
         id="password"
         type="password"
@@ -38,13 +50,13 @@ const form = ref({
         type="button"
         class="inline-flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-full hover:bg-indigo-700 md:py-2 md:text-lg md:px-10 hover:shadow"
       >
-        Sign In
+        Continue Sign Up
       </button>
       <RouterLink
-       to="/register"
+        to="/login"
         class="inline-flex items-center justify-center w-full px-8 py-3 mt-2 text-base font-medium text-black bg-gray-200 border border-transparent rounded-full hover:bg-gray-300 md:py-2 md:text-lg md:px-10 hover:shadow"
       >
-        Create New Account
+        Sign In
       </RouterLink>
     </div>
   </form>
